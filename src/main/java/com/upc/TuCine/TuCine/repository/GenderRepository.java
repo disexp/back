159f8c636c7bc8225d9fc5b60dec7ad0fb8a907f
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-06-08 10:30:50 -0500

    GenderRepository created

---
+++ src/main/java/com/upc/TuCine/TuCine/repository/GenderRepository.java
@@ -0,0 +1,7 @@
+package com.upc.TuCine.TuCine.repository;
+
+import com.upc.TuCine.TuCine.model.Gender;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenderRepository extends JpaRepository<Gender,Integer> {
+}
