99e887da9c92f14e5a62fcf2814aba4158d7f25b
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-06-09 16:08:36 -0500

    First Version of POST

--- src/main/java/com/upc/TuCine/TuCine/controller/FilmController.java
+++ src/main/java/com/upc/TuCine/TuCine/controller/FilmController.java
@@ -1,6 +1,8 @@
 package com.upc.TuCine.TuCine.controller;
 
+import com.upc.TuCine.TuCine.model.Category;
 import com.upc.TuCine.TuCine.model.Film;
+import com.upc.TuCine.TuCine.repository.CategoryRepository;
 import com.upc.TuCine.TuCine.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,9 +17,11 @@ import java.util.List;
 public class FilmController {
     @Autowired
     private FilmRepository filmRepository;
-
-    public FilmController(FilmRepository filmRepository) {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    public FilmController(FilmRepository filmRepository, CategoryRepository categoryRepository) {
         this.filmRepository = filmRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     //URL: http://localhost:8080/api/TuCine/v1/films
@@ -34,6 +38,22 @@ public class FilmController {
     @PostMapping("/films")
     public ResponseEntity<Film> createFilm(@RequestBody Film film){
         return new ResponseEntity<Film>(filmRepository.save(film), HttpStatus.CREATED);
+    }
+
+    // URL: http://localhost:8080/api/TuCine/v1/films/{filmId}/categories/{categoryId}
+    // Method: POST
+    @Transactional
+    @PostMapping("/films/{filmId}/categories/{categoryId}")
+    public ResponseEntity<?> addCategoryToFilm(@PathVariable("filmId") Integer filmId, @PathVariable("categoryId") Integer categoryId) {
+        Film film = filmRepository.findById(filmId).orElse(null); // Obtener el film por su ID
+        Category category = categoryRepository.findById(categoryId).orElse(null); // Obtener la categoría por su ID
+
+        if (film == null || category == null) {
+            return ResponseEntity.notFound().build(); // Manejar casos en los que no se encuentre el film o la categoría
+        }
+        film.getCategories().add(category); // Agregar la categoría al film
+        filmRepository.save(film); // Guardar los cambios en la base de datos
+        return ResponseEntity.ok().build();
     }
 
 }
