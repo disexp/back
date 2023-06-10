9f2fd9d5d2b1a7a82d261760ba82b1f7e9a36c4f
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-06-09 20:56:37 -0500

    Creation of Post method Customer

---
+++ src/main/java/com/upc/TuCine/TuCine/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.upc.TuCine.TuCine.controller;
+
+import com.upc.TuCine.TuCine.model.Customer;
+import com.upc.TuCine.TuCine.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/TuCine/v1")
+public class CustomerController {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    //URL: http://localhost:8080/api/TuCine/v1/customers
+    //Method: POST
+    @Transactional
+    @PostMapping("/customers")
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+        return new ResponseEntity<Customer>(customerRepository.save(customer), HttpStatus.CREATED);
+    }
+}
