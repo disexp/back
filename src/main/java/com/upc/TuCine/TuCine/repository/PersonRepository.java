3b60d8567f7dee61fe3178e7538c07cd41d0abc0
YeniAlvarado <u20201b032@upc.edu.pe>
2023-06-10 23:50:10 -0500

    Implement
    method findById Person

--- src/main/java/com/upc/TuCine/TuCine/repository/PersonRepository.java
+++ src/main/java/com/upc/TuCine/TuCine/repository/PersonRepository.java
@@ -2,7 +2,15 @@ package com.upc.TuCine.TuCine.repository;
 
 import com.upc.TuCine.TuCine.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PersonRepository extends JpaRepository<Person,Integer> {
+    Optional<Person> findById(Long personId);
 }
+
+
+
 
