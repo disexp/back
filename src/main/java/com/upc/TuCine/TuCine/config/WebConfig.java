9dfeec7356a6ca7df7db7e41bcdc335ef97f2a7c
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-06-28 00:33:15 -0500

    fix: Update WebConfig

--- src/main/java/com/upc/TuCine/TuCine/config/WebConfig.java
+++ src/main/java/com/upc/TuCine/TuCine/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.upc.TuCine.TuCine.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.RedirectViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,8 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://frontend-tucine-production.up.railway.app")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        RedirectViewControllerRegistration redirectViewControllerRegistration = registry.addRedirectViewController("/", "https://backend-tucine-production.up.railway.app/swagger-ui/index.html");
+        redirectViewControllerRegistration.setKeepQueryParams(true);
+        redirectViewControllerRegistration.setStatusCode(HttpStatusCode.valueOf(302));
     }
 }
