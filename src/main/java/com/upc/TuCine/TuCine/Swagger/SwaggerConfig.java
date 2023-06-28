e508c0f8439e9c958d49eb63bf1bf4aa3d6c8e51
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-06-28 00:14:23 -0500

    update SwaggerConfig

--- src/main/java/com/upc/TuCine/TuCine/Swagger/SwaggerConfig.java
+++ src/main/java/com/upc/TuCine/TuCine/Swagger/SwaggerConfig.java
@@ -2,13 +2,16 @@ package com.upc.TuCine.TuCine.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition
 public class SwaggerConfig {
 
-    public OpenAPI api(){
-        return new OpenAPI();
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI().addServersItem(new Server().url("http://backend-tucine-production.up.railway.app"));
     }
 }
